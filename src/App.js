3017963b9a2b2dd9386994f1e6fa67d123704330
wonderfulkasey <kaseyarnold15@gmail.com>
2021-02-22 15:50:41 -0500

    testing the console log

--- src/App.js
+++ src/App.js
@@ -2,6 +2,15 @@ import React from 'react';
 
 
 class App extends React.Component {
+ 
+ componentDidMount() {
+  fetch('http://localhost:3000/api/v1/characters', {
+    method: 'GET'
+  })
+  .then(response => response.json())
+  .then(data => console.log(data))
+  }
+ 
   render() {
     return (
       <div className="App">
