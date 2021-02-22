c55e365751d57a56517473bd720f11c6f2a7e0eb
wonderfulkasey <kaseyarnold15@gmail.com>
2021-02-22 16:24:17 -0500

    got rid of console log component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,6 @@ import React from 'react';
 
 class App extends React.Component {
  
- componentDidMount() {
-  fetch('http://localhost:3000/api/v1/characters', {
-    method: 'GET'
-  })
-  .then(response => response.json())
-  .then(data => console.log(data))
-  }
  
   render() {
     return (
