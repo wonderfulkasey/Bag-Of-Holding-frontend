0416a54f3914db9f2debeaa4a5cf389150990ba7
wonderfulkasey <kaseyarnold15@gmail.com>
2021-02-24 13:13:11 -0500

    added char container to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {fetchCharacters} from './actions/fetchCharacters'
+import CharactersContainer from './containers/CharactersContainer.js'
 
 
 class App extends React.Component {
  
-  
+
   render() {
     return (
       <div className="App">
--- src/containers/CharactersContainer.js
+++ src/containers/CharactersContainer.js
@@ -3,5 +3,15 @@ import React from 'react'
 
 class CharactersContainer extends React.Component {
 
-    
+    render() {
+        return (
+            <div>
+            CharactersContainer
+            </div>
+        )
+    }
+
 }
+
+
+export default CharactersContainer
