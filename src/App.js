102876900ccbcbb7b2c2d79f2342de2dbcbca423
wonderfulkasey <kaseyarnold15@gmail.com>
2021-02-24 11:45:33 -0500

    added a fetch and console log

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import React from 'react';
 
 class App extends React.Component {
  
+componentDidMount() {
+  fetch('http://localhost:3000/api/v1/characters')
+  .then(resp => resp.json())
+  .then(data => console.log(data))
+}
  
   render() {
     return (
