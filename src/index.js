651dec72157b1559649ae8c88d13c488e0080ed4
wonderfulkasey <kaseyarnold15@gmail.com>
2021-02-24 11:33:36 -0500

    changed to charreducer in index

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 import { Provider } from 'react-redux'
+import characterReducer from './reducers/characterReducer'
 
 import App from './App';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 //set up our store
-let store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)))
+let store = createStore(characterReducer, composeEnhancers(applyMiddleware(thunk)))
 
 
 
