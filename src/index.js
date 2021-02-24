8b4712e792bbb9b72b5104b1d5a1500c1208ef0f
wonderfulkasey <kaseyarnold15@gmail.com>
2021-02-24 11:04:02 -0500

    adding createstore

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import {createStore, applyMiddleware} from 'redux';
-//import thunk from 'redux-thunk';
-//import {Provider} from 'react-redux';
+import {createStore, applyMiddleware, compose} from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
 
 import App from './App';
 
-//set up our store
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let reducer = " "
+
+//set up our store
+let store = createStore(reducer, applyMiddleware(thunk))
+
+
+
+
+ReactDOM.render(
+    <Provider >
+        <App />
+    </Provider>
+        ,
+document.getElementById('root'));
