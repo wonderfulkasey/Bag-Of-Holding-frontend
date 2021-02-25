64b7fc3b99cad5a09b4caf0167e9f0c6d98cc9c2
wonderfulkasey <kaseyarnold15@gmail.com>
2021-02-25 12:03:40 -0500

    adding handle change and handle submit in form

--- src/components/CharacterInput.js
+++ src/components/CharacterInput.js
@@ -4,14 +4,26 @@ import React from 'react'
 
 class CharacterInput extends React.Component {
 
+    state = {name: ''}
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        this.props.addCharacter(this.state)
+    }
+
     render() {
         return (
             <div>
                 <form>
                     <label> Character Name</label> <br/>
-                    <input type='text' placeholder='Name'>
-
-                    </input>
+                    <input type='text' placeholder='Name' value={this.state.name} name="name" onChange={this.handleChange}/>
+                    <br/> <br/>
                 </form>
             </div>
         )
