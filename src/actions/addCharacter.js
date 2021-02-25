c59f6b4cab28b748347a29f4d342b0fbbf298c7a
wonderfulkasey <kaseyarnold15@gmail.com>
2021-02-25 12:25:07 -0500

    added addchar function

---
+++ src/actions/addCharacter.js
@@ -0,0 +1,16 @@
+export const addCharacter = (data) => {
+
+    return (dispatch) => {
+        fetch('http://localhost:3000/api/v1/characters', {
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            method: 'POST',
+            body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+        .then(character => dispatch({type: 'ADD_CHARACTER', payload: character}))
+    }
+
+}
