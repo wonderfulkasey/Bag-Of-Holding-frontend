1a86c0f6eadd1b4437c618a85826cbc5d3c54e76
wonderfulkasey <kaseyarnold15@gmail.com>
2021-02-25 12:20:02 -0500

    added add char to the reducer

--- src/reducers/characterReducer.js
+++ src/reducers/characterReducer.js
@@ -6,6 +6,8 @@ export default function characterReducer(state = {characters: []}, action) {
     switch (action.type) {
         case 'FETCH_CHARACTERS':
             return {characters: action.payload}
+        case 'ADD_CHARACTER':
+            return {...state, characters: [...state.characters, action.payload]}
         default:
             return state
     }
