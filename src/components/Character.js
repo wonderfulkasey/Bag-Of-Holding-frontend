7ef3e7b29ceb74fa8c3699e590e6d29feaf1f801
wonderfulkasey <kaseyarnold15@gmail.com>
2021-02-26 14:02:30 -0500

    adding const to char js for individual chars

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,3 +1,18 @@
 import React from 'react'
 
+
+const Character = (props) => {
+
+    console.log(props)
+
+    let character = props.characters[props.match.params.id - 1]
+    console.log(character)
+
+    return (
+        <li>
+            {character ? character.name : null}
+        </li>
+    )
+}
+
 export default Character
