b93dc16edaadaa6e375355e15025a2b6176bbf8d
wonderfulkasey <kaseyarnold15@gmail.com>
2021-02-26 15:22:57 -0500

    added a bags controller

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,5 +1,7 @@
 import React from 'react'
+import {Redirect} from 'react-router-dom'
 
+import BagsContainer from '../containers/BagsContainer'
 
 const Character = (props) => {
 
@@ -7,9 +9,9 @@ const Character = (props) => {
     console.log(character)
 
     return (
-        <li>
+        <h3>
          {character ? character.name : null}
-        </li>
+        </h3>
     )
 }
 
