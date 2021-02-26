3fc29799f350c463ad1d26a20b7786df87be8174
wonderfulkasey <kaseyarnold15@gmail.com>
2021-02-26 15:51:02 -0500

    added more to bags container

--- src/containers/BagsContainer.js
+++ src/containers/BagsContainer.js
@@ -2,4 +2,15 @@ import React from 'react'
 import BagInput from '../components/BagInput'
 import Bags from '../components/Bags'
 
+class BagsContainer extends React.Component {
+    render() {
+        return (
+            <div>
+                <BagInput/>
+                <Bags bags={this.props.character && this.props.character.bags}/>
+            </div>
+        )
+    }
+}
+
 export default BagsContainer
