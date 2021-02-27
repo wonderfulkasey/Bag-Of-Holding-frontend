a4a6915f37e88d4dfd3c1b3ae2ba0ddf361c55d9
wonderfulkasey <kaseyarnold15@gmail.com>
2021-02-27 13:51:43 -0500

    added the add bag case to reducer action

--- src/reducers/characterReducer.js
+++ src/reducers/characterReducer.js
@@ -8,6 +8,16 @@ export default function characterReducer(state = {characters: []}, action) {
             return {characters: action.payload}
         case 'ADD_CHARACTER':
             return {...state, characters: [...state.characters, action.payload]}
+        case 'ADD_BAG':
+            let characters = state.characters.map(character => {
+                if (character.id === action.payload.id) {
+                    return action.payload
+                } else {
+                    return character
+                }
+            })
+            return {...state, characters: characters}
+        
         default:
             return state
     }
