60842ecfc2958c051bfdca0fdb9a5c585321ad85
wonderfulkasey <kaseyarnold15@gmail.com>
2021-02-27 14:24:52 -0500

    finished the addbag, so we can use put thru input

--- src/actions/addBag.js
+++ src/actions/addBag.js
@@ -1,3 +1,16 @@
 export const addBag = (bag, characterId) => {
-    
+
+    return (dispatch) => {
+        fetch(`http://localhost:3000/api/v1/characters/${characterId}/bags`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(bag)
+        })
+        .then(response => response.json())
+        .then(character => dispatch({type: 'ADD_BAG', payload: character}
+        ))
+    }
+
 }
