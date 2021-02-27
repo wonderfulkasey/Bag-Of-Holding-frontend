a3c1c7162544b9847d41763e111c2512d64dd5f7
wonderfulkasey <kaseyarnold15@gmail.com>
2021-02-27 14:47:06 -0500

    added a full bag input form, and it saves the bag

--- src/components/BagInput.js
+++ src/components/BagInput.js
@@ -3,10 +3,38 @@ import {connect} from 'react-redux'
 import {addBag} from '../actions/addBag'
 
 class BagInput extends React.Component {
+
+    state = {
+        title: '',
+        description: ''
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        this.props.addBag(this.state, this.props.character.id)
+        this.setState({
+            title: '',
+            description: ''
+        })
+    }
+    
     render () {
         return (
             <div>
-                bag input
+                <form onSubmit={this.handleSubmit}>
+                    <label>Bag Title:</label>
+                    <input type="text" name="title" value={this.state.title} onChange={this.handleChange}/>
+                    <br/>
+                    <label>Bag Description:</label>
+                    <input type="text" name="description" value={this.state.description} onChange={this.handleChange}/>
+                    <input type="submit"/>
+                </form>
             </div>
         )
     }
