e4ef67f6be4d185318eeadfb9c369d41311cbab9
wonderfulkasey <kaseyarnold15@gmail.com>
2021-03-01 13:15:19 -0500

    added rest of characteredit to file

---
+++ src/components/CharacterEdit.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {editCharacter} from '../actions/editCharacter'
+
+class CharacterEdit extends React.Component {
+
+    state = {
+        name: ''
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        let account = {...this.state, id: this.props.character.id}
+        this.props.editCharacter(character)
+        this.setState({
+            name: ''
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <label>Character Name</label>
+                    <input type='text' placeholder='Name' value={this.state.name} name="name" onChange={this.handleChange}/>
+                    <br/>
+                    <input type="submit"/>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default connect(null, {editCharacter})(CharacterEdit)
