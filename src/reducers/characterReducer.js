0970a03247a567253ef77ea57b9742dce6e3dc0d
wonderfulkasey <kaseyarnold15@gmail.com>
2021-03-01 15:44:27 -0500

    made charreducer easier to read

--- src/reducers/characterReducer.js
+++ src/reducers/characterReducer.js
@@ -1,13 +1,12 @@
-
-
-
 export default function characterReducer(state = {characters: []}, action) {
 
     switch (action.type) {
         case 'FETCH_CHARACTERS':
             return {characters: action.payload}
+
         case 'ADD_CHARACTER':
             return {...state, characters: [...state.characters, action.payload]}
+        
         case 'ADD_BAG':
             let characters = state.characters.map(character => {
                 if (character.id === action.payload.id) {
@@ -17,6 +16,7 @@ export default function characterReducer(state = {characters: []}, action) {
                 }
             })
             return {...state, characters: characters}
+        
         case 'DELETE_BAG':
             let charactersTwo = state.characters.map(character => {
                 if (character.id === action.payload.id) {
@@ -26,6 +26,7 @@ export default function characterReducer(state = {characters: []}, action) {
                 }
             })
             return {...state, characters: charactersTwo}
+        
         case 'EDIT_CHARACTER':
             let charactersThree = state.characters.map(character => {
                 if (character.id === action.payload.id) {
