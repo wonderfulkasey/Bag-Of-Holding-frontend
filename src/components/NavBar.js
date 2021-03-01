1e7893c11254d348953d9963b82597964e9c8882
wonderfulkasey <kaseyarnold15@gmail.com>
2021-03-01 15:04:14 -0500

    added links to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,25 @@
+
+import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react'
 import {Link} from 'react-router-dom'
+
 
 const NavBar = (props) => {
 
     return (
-        <div>
-            <Link to='/characters'>Characters</Link>
-            <Link to='/characters/new'>Add Character</Link>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+          <a class="navbar-brand">BAG OF HOLDING</a>
+          <a class="navbar-brand" href="/characters">All Characters</a>
+          
+          <a class="navbar-brand" href="/characters/new">New Character</a>
+         
         </div>
+      </nav>
+    
+
+
+        
     )
 }
 
