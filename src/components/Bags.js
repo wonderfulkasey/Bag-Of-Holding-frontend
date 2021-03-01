cf437a1fdc7e6e75afd0f061c15c19a4141a58c1
wonderfulkasey <kaseyarnold15@gmail.com>
2021-03-01 14:18:29 -0500

    added a delete button for bags

--- src/components/Bags.js
+++ src/components/Bags.js
@@ -2,23 +2,31 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {deleteBag} from '../actions/deleteBag'
 
-const Bags = (props) => {
+class Bags extends React.Component {
 
-    const handleDelete = (bag) => {
-        props.deleteBag(bag.id, bag.character_id)
+    state = {}
+
+    handleDelete = (bag) => {
+        this.props.deleteBag(bag.id, bag.character_id)
     }
 
-    return (
-        <div>
-            <br/>
-           {props.bags && props.bags.map(bag =>
-                <li key={bag.id}> Bag: {bag.title} <br/> 
-                Description: "{bag.description}" <br/>
-                <button onClick={() => handleDelete(bag)}> Delete Bag </button>
-                </li>
-            )}
-        </div>
-    )
+    vote = (id) => {
+        this.state[id] ? this.setState({[id]: this.state[id] += 1}) : this.setState({[id]: 1})
+    }
+
+    render() {
+        return (
+            <div>
+                {this.props.bags && this.props.bags.map( bag =>
+                    <li key={bag.id}>{bag.title} - {bag.description} 
+                        <button onClick={() => this.handleDelete(bag)}>
+                            Delete
+                        </button>
+                    </li>
+                )}
+            </div>
+        )
+    }  
 }
 
 export default connect(null, {deleteBag})(Bags)
