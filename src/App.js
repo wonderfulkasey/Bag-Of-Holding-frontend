8f0984ee576333ff88f7521123f4e26e5ee53017
wonderfulkasey <kaseyarnold15@gmail.com>
2021-03-29 11:36:16 -0400

    added new imports and routes to dash and pref

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import {connect} from 'react-redux'
 import CharactersContainer from './containers/CharactersContainer'
-
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Dashboard from '../Dashboard/Dashboard';
+import Preferences from '../Preferences/Preferences';
 
 class App extends React.Component {
  
@@ -9,6 +11,18 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <h1>Application</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/preferences">
+            <Preferences />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+          
        <CharactersContainer/>
       </div>
     )
