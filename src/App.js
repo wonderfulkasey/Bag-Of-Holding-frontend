605d9f1b59f269a4bd6f55f360ddb8af1144d963
wonderfulkasey <kaseyarnold15@gmail.com>
2021-03-29 11:40:56 -0400

    moving around files, adding a dashboard container

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import React from 'react';
 import {connect} from 'react-redux'
 import CharactersContainer from './containers/CharactersContainer'
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Dashboard from '../Dashboard/Dashboard';
-import Preferences from '../Preferences/Preferences';
+import Dashboard from './containers/DashboardContainer';
 
 class App extends React.Component {
  
@@ -11,18 +10,8 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <h1>Application</h1>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/preferences">
-            <Preferences />
-          </Route>
-        </Switch>
-      </BrowserRouter>
-          
+        <DashboardContainer/>
+
        <CharactersContainer/>
       </div>
     )
